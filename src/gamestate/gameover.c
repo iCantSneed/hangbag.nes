115c228e088f7bbf454b8f2a8a21c680eecc25c1
iCantSneed <140197533+iCantSneed@users.noreply.github.com>
2025-08-14 23:21:14 -0600

    Add game over screen - part 2

--- src/gamestate/gameover.c
+++ src/gamestate/gameover.c
@@ -6,8 +6,9 @@ void fastcall gamestate_gameover_wait();
 
 const unsigned char gameover_palette[16] = {
   0x0f, 0x06, 0x16, 0x07,
-  0,    0x1c, 0x0c, 0,
-  0,    0x27, 0,    0,
+  0,    0x1c, 0x0c, 0x11,
+  0,    0x27, 0x17, 0,
+  0,    0x07, 0x17, 0x37,
 };
 
 void fastcall gamestate_gameover_init()
