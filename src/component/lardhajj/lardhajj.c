223c3674b51810f3cf420d87a577241ec4638b25
iCantSneed <140197533+iCantSneed@users.noreply.github.com>
2025-08-25 13:24:27 -0600

    Implement lardhajj and level 4

--- src/component/lardhajj/lardhajj.c
+++ src/component/lardhajj/lardhajj.c
@@ -3,55 +3,93 @@
 
 #pragma bss-name (push,"ZEROPAGE")
 
-unsigned char lardhajj_x;
-char lardhajj_delta;
+unsigned int lardhajj_x;
+unsigned int lardhajj_delta;
+static unsigned char metaspr_idx;
 
 #pragma bss-name (push,"RODATA")
 
-const unsigned char lardhajj_metaspr[] = {
-  0, 0, 0x50, 0,
-  8, 0, 0x52, 0,
-  16, 0, 0x54, 0,
-  24, 0, 0x56, 0,
-  0, 16, 0x58, 0,
-  8, 16, 0x5a, 0,
-  16, 16, 0x5c, 0,
-  24, 16, 0x5e, 0,
-  128
+#define LARDHAJJ_THICCNESS 29
+#define X_OFF -16
+#define Y_OFF -10
+#define LARDHAJJ_RIGHT_METASPR(walk_idx) { \
+  X_OFF + 8*0, Y_OFF + 16*0, 0x50, 0, \
+  X_OFF + 8*1, Y_OFF + 16*0, 0x52, 0, \
+  X_OFF + 8*2, Y_OFF + 16*0, 0x54, 0, \
+  X_OFF + 8*3, Y_OFF + 16*0, 0x56, 0, \
+  X_OFF + 8*0, Y_OFF + 16*1, 0x58, 0, \
+  X_OFF + 8*1, Y_OFF + 16*1, 0x5a, 0, \
+  X_OFF + 8*2, Y_OFF + 16*1, walk_idx, 0, \
+  X_OFF + 8*3, Y_OFF + 16*1, 0x5e, 0, \
+  128 \
+}
+#define LARDHAJJ_LEFT_METASPR(walk_idx) { \
+  -LARDHAJJ_THICCNESS - X_OFF + 8*0, Y_OFF + 16*0, 0x56, OAM_FLIP_H, \
+  -LARDHAJJ_THICCNESS - X_OFF + 8*1, Y_OFF + 16*0, 0x54, OAM_FLIP_H, \
+  -LARDHAJJ_THICCNESS - X_OFF + 8*2, Y_OFF + 16*0, 0x52, OAM_FLIP_H, \
+  -LARDHAJJ_THICCNESS - X_OFF + 8*3, Y_OFF + 16*0, 0x50, OAM_FLIP_H, \
+  -LARDHAJJ_THICCNESS - X_OFF + 8*0, Y_OFF + 16*1, 0x5e, OAM_FLIP_H, \
+  -LARDHAJJ_THICCNESS - X_OFF + 8*1, Y_OFF + 16*1, walk_idx, OAM_FLIP_H, \
+  -LARDHAJJ_THICCNESS - X_OFF + 8*2, Y_OFF + 16*1, 0x5a, OAM_FLIP_H, \
+  -LARDHAJJ_THICCNESS - X_OFF + 8*3, Y_OFF + 16*1, 0x58, OAM_FLIP_H, \
+  128 \
+}
+const unsigned char lardhajj_metaspr_right_1[] = LARDHAJJ_RIGHT_METASPR(0x5c);
+const unsigned char lardhajj_metaspr_right_2[] = LARDHAJJ_RIGHT_METASPR(0x4e);
+const unsigned char lardhajj_metaspr_left_1[] = LARDHAJJ_LEFT_METASPR(0x5c);
+const unsigned char lardhajj_metaspr_left_2[] = LARDHAJJ_LEFT_METASPR(0x4e);
+const unsigned char *lardhajj_metaspr[] = {
+  lardhajj_metaspr_right_1, lardhajj_metaspr_left_1, lardhajj_metaspr_right_2, lardhajj_metaspr_left_2
 };
 
-#define LARDHAJJ_LEFT_X (8*2+14-5)
-#define LARDHAJJ_RIGHT_X (8*30-10)
-#define LARDHAJJ_GROUND_POS (8*24)
+#define LARDHAJJ_HEIGHT 32
+
+#define LARDHAJJ_LEFT_X (8*2 - X_OFF)
+#define LARDHAJJ_RIGHT_X (8*30 - X_OFF - LARDHAJJ_THICCNESS)
+#define LARDHAJJ_GROUND_POS (8*28 - Y_OFF - LARDHAJJ_HEIGHT)
 
 void fastcall lardhajj_reset(void)
 {
-  lardhajj_x = 0x80;
-  lardhajj_delta = -1;
+  lardhajj_x = 0x8000;
+  lardhajj_delta = -0x80;
 }
 
 void fastcall lardhajj_tick(void)
 {
   lardhajj_x += lardhajj_delta;
-  if (lardhajj_x <= LARDHAJJ_LEFT_X)
+  if (MSB(lardhajj_x) < LARDHAJJ_LEFT_X)
   {
-    lardhajj_x = LARDHAJJ_LEFT_X;
+    lardhajj_x = LARDHAJJ_LEFT_X << 8;
     lardhajj_delta = -lardhajj_delta;
   }
-  else if (lardhajj_x >= LARDHAJJ_RIGHT_X)
+  else if (MSB(lardhajj_x) >= LARDHAJJ_RIGHT_X)
   {
-    lardhajj_x = LARDHAJJ_RIGHT_X;
+    lardhajj_x = LARDHAJJ_RIGHT_X << 8;
     lardhajj_delta = -lardhajj_delta;
   }
 }
 
 NooseState fastcall lardhajj_check_collide(void)
 {
-  // TODO
+  if (
+    noose_y >= LARDHAJJ_GROUND_POS - 8 &&
+    noose_y <= LARDHAJJ_GROUND_POS + 8 &&
+    noose_x >= MSB(lardhajj_x) - 8 &&
+    noose_x <= MSB(lardhajj_x) + 8
+  )
+  {
+    return NOOSE_STATE_HURT;
+  }
   return NOOSE_STATE_UNCHANGED;
 }
 
 void fastcall lardhajj_render(void)
 {
-  gfx_oam_metaspr(lardhajj_x, LARDHAJJ_GROUND_POS, lardhajj_metaspr);
+  metaspr_idx = ((MSB(lardhajj_x) >> 1) & 2);
+  metaspr_idx |= (MSB(lardhajj_delta) >= 0x80);
+  gfx_oam_metaspr(
+    MSB(lardhajj_x),
+    LARDHAJJ_GROUND_POS,
+    lardhajj_metaspr[metaspr_idx]
+  );
 }
